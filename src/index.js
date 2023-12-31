76c1f0a136a56ff7a9c82172c91641b7d125f70e
Ashutosh Kumar <75922918+ashusnapx@users.noreply.github.com>
2023-12-31 18:45:10 +0530

    worked on api error and response class

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 
-dotenv.config( {
-    path: './env'
-} )
+dotenv.config({
+  path: "./env",
+});
 
 connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log(`MongoDB connection error occured : `, err));
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+// method - 2
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
+};
+
+// method - 1
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
+
+export default asyncHandler;
