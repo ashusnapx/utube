709e51a526a317e6e0697eb08789e916dc3bb65b
Ashutosh Kumar <75922918+ashusnapx@users.noreply.github.com>
2024-01-03 08:19:46 +0530

    added routes and controllers for user

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,6 @@
 // method - 2
 const asyncHandler = (requestHandler) => {
-  (req, res, next) => {
+  return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((error) =>
       next(error)
     );
@@ -19,4 +19,4 @@ const asyncHandler = (requestHandler) => {
 //   }
 // };
 
-export default asyncHandler;
+export { asyncHandler };
